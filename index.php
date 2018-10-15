008e0f35fcbedc750cbaef502e68579b0624e915
Michel <Michel.Marjamaa@gmail.com>
2018-10-15 08:12:30 +0300

    -edit

--- index.php
+++ index.php
@@ -16,89 +16,6 @@
 
 </head>
 <body>
-    <!-- Facebook API script and functions-->
-    <script>
-        window.fbAsyncInit = function() {
-            FB.init({
-                appId      : '559098287881416',
-                cookie     : true,
-                xfbml      : true,
-                version    : 'v3.1'
-            });
-
-
-            FB.getLoginStatus(function(response) {
-                statusChangeCallback(response);
-            });
-
-        };
-
-        (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-
-        function statusChangeCallback(response) {
-            if (response.status === 'connected'){
-                setElements(true);
-                fetchAPI();
-                console.log("Logged in");
-            }
-            else {
-                setElements(false);
-                console.log("Not loged in");
-            }
-        }
-
-        function checkLoginState() {
-            FB.getLoginStatus(function(response) {
-                statusChangeCallback(response);
-            });
-        }
-
-        function setElements(loginInfo){
-            if (loginInfo){
-                document.getElementById("settings").style.display = "block";
-                document.getElementById("logout").style.display = "block";
-                document.getElementById("jumbotron_logged_out").style.display = "none";
-                document.getElementById("jumbotron_logged_in").style.display = "block";
-                document.getElementById("fb_login").style.display = "none";
-            }
-            else {
-                document.getElementById("settings").style.display = "none";
-                document.getElementById("logout").style.display = "none";
-                document.getElementById("jumbotron_logged_in").style.display = "none";
-                document.getElementById("jumbotron_logged_out").style.display = "block";
-                document.getElementById("fb_login").style.display = "block";
-            }
-        }
-
-        function logout(){
-            FB.logout(function(response){
-                setElements(false);
-            });
-        }
-
-        function fetchAPI(){
-            FB.api("/me?fields=id,name", function(response) {
-                if (response && !response.error) {
-                    userDetails(response)
-                }
-            });
-        }
-
-        function userDetails(user){
-            let name = "<h1>$(user.name)</h1>";
-            let id = $(user.id);
-
-            document.getElementById("name_present").innerHTML = name;
-        }
-
-
-    </script>
 
 
     <header>
@@ -221,6 +138,74 @@
     </footer>
     <div class="container-fluid">
     </div>
+
+    <!-- Facebook API script and functions-->
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '559098287881416',
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v3.1'
+            });
+
+
+            FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+            });
+
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+        function statusChangeCallback(response) {
+            if (response.status === 'connected'){
+                setElements(true);
+                console.log("Logged in");
+            }
+            else {
+                setElements(false);
+                console.log("Not loged in");
+            }
+        }
+
+        function checkLoginState() {
+            FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+            });
+        }
+
+        function setElements(loginInfo){
+            if (loginInfo){
+                document.getElementById("settings").style.display = "block";
+                document.getElementById("logout").style.display = "block";
+                document.getElementById("jumbotron_logged_out").style.display = "none";
+                document.getElementById("jumbotron_logged_in").style.display = "block";
+                document.getElementById("fb_login").style.display = "none";
+            }
+            else {
+                document.getElementById("settings").style.display = "none";
+                document.getElementById("logout").style.display = "none";
+                document.getElementById("jumbotron_logged_in").style.display = "none";
+                document.getElementById("jumbotron_logged_out").style.display = "block";
+                document.getElementById("fb_login").style.display = "block";
+            }
+        }
+
+        function logout(){
+            FB.logout(function(response){
+                setElements(false);
+            });
+        }
+
+
+    </script>
 
 </body>
 </html>
