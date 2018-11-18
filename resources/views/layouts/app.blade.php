afc8f8ee7e2645866798c925de58c9a1b1303483
Sten Vaher <stenvaher@hotmail.com>
2018-11-18 23:25:19 +0200

    revert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css'); }}" />
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
     <style media="screen">
         #fb_login{margin-top: 12px}
     </style>
