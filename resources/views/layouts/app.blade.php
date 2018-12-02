cac83deb323620272e1b86591b07409e492c5742
Sten Vaher <stenvaher@hotmail.com>
2018-12-02 14:17:48 +0200

    Revert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>  
     <link href="{{ secure_asset('css/style.css') }}" media="all" rel="stylesheet" type="text/css" />
     <style media="screen">
         #fb_login{margin-top: 12px}
